f1c3fa6c6061c6dbd3b5751fd8fc5ef96198360e
wilsonpang <93904203+wilsonpang@users.noreply.github.com>
2022-05-05 16:18:21 +0800

    [ADD] pos print test
    build function test

---
+++ pos_android-print/static/src/js/models.js
@@ -0,0 +1,10 @@
+odoo.define('pult_pos.main2', function (require) {
+    "use strict";
+    var models = require('point_of_sale.models');
+
+    models.Orderline = models.Orderline.extend({
+        get_display_price: function(){
+            return this.get_base_price();
+            },
+        });
+});
