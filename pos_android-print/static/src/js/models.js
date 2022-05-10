e061898e16218ef6061a5d77ae68619b567317f4
Eric Leung <ericleung@obhgroupltd.com>
2022-05-10 11:32:44 +0800

    pos print test

--- pos_android-print/static/src/js/models.js
+++ pos_android-print/static/src/js/models.js
@@ -1,11 +1,11 @@
-odoo.define('pult_pos.main2', function (require) {
+odoo.define('pos_custom.pos_custom', function (require) {
     "use strict";
     var models = require('point_of_sale.models');
-
+    var _super_orderline = models.Orderline.prototype;
     models.Orderline = models.Orderline.extend({
-        get_display_price: function(){
-            console.log("custom code ran")
-            return this.get_base_price();
-            },
-        });
+        get_display_price: function () {
+            var price = _super_orderline.get_display_price.call(this);//old_price
+            return price*2;//custom price
+        }
+    });
 });
