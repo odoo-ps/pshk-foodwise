ae0928ed4958d95fa7210f040627ffb8a15dfd10
Eric Leung <ericleung@obhgroupltd.com>
2022-05-17 13:36:46 +0800

    pos rebuild

--- pos_android-print/README.md
+++
@@ -1,7 +0,0 @@
-# Pos Android Print
-
-## [15.0.1.0.0]
-
--   Enable Sunmi Printing
-
-2697632
--- pos_android-print/static/src/js/models.js
+++
@@ -1,5 +0,0 @@
-odoo.define('pos_custom.pos_custom', function (require) {
-    "use strict";
-    var models = require('point_of_sale.models');
-
-});
